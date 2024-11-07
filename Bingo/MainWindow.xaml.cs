c7aad6a9b751d3975b5b3246712d30df751ecb3f
psytoh <uuk65536@gmail.com>
2024-11-07 12:41:26 +0900

    機能追加 - マスの選択をできるように - ビンゴを判定できるように - 生成したカードの履歴をハッシュで記録し過去に出たパターンとの重複が発生しないように

--- Bingo/MainWindow.xaml.cs
+++ Bingo/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bingo
 {
@@ -17,64 +10,123 @@ namespace Bingo
     public partial class MainWindow : Window
     {
         private const int grid_count = 25;
-        private readonly List<int> history = [];
+        private bool[] table_state = new bool[grid_count];
         private readonly Random rnd_gen = new();
+        private readonly List<int> table_history = [];
         public MainWindow()
         {
             InitializeComponent();
             for (int i = 0; i < grid_count; i++)
             {
                 var child = VisualTreeHelper.GetChild(gdBingo, i);
-                if (child != null && child is Label label)
+                if (child != null && child is Button button)
                 {
-                    label.Content = label.Name;
+                    button.Content = button.Name;
                 }
+                lbHash.Content = "Hash";
             }
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Shuffle(object sender, RoutedEventArgs e)
         {
             int[] bingo = new int[grid_count];
+            int bingo_hash;
 
-            for (int i = 0; i < bingo.Length; i++)
+            while (true)
             {
-                int rnd;
-                do
+                for (int i = 0; i < bingo.Length; i++)
                 {
-                    rnd = (int)rnd_gen.NextInt64(1, 75 + 1);
-                } while (bingo.Contains(rnd));
-                bingo[i] = rnd;
+                    int rnd;
+                    do
+                    {
+                        rnd = (int)rnd_gen.NextInt64(1, 75 + 1);
+                    } while (bingo.Contains(rnd));
+                    bingo[i] = rnd;
+                }
+                bingo_hash = bingo.GetHashCode();
+                if (!table_history.Contains(bingo_hash)) break;
             }
+
+            table_history.Add(bingo_hash);
+            lbHash.Content = bingo_hash;
 
             for (int i = 0; i < grid_count; i++)
             {
                 var child = VisualTreeHelper.GetChild(gdBingo, i);
-                if (child != null && child is Label label)
+                if (child != null && child is Button button)
                 {
-                    label.Content = bingo[i];
+                    button.Content = bingo[i];
+                    button.Background = new BrushConverter().ConvertFrom("#FFDDDDDD") as SolidColorBrush;
                 }
             }
 
-            history.Clear();
+            table_state = new bool[grid_count];
+            lvHistory.Items.Clear();
         }
-        private void LabelClicked(object sender, RoutedEventArgs e)
+
+        private void TileClicked(object sender, RoutedEventArgs e)
         {
-            if (sender != null && sender is Label label)
+            if (sender != null && sender is Button button)
             {
-                int row = int.Parse(label.Name[2].ToString());
-                int col = int.Parse(label.Name[3].ToString());
+                int row = int.Parse(button.Name[2].ToString());
+                int col = int.Parse(button.Name[3].ToString());
                 int number = row * 5 + col;
 
-                history.Add(number);
-
-                string history_str = "";
-                foreach (int item in history)
+                if (!table_state[number])
                 {
-                    history_str += item.ToString() + ", ";
+                    table_state[number] = true;
+
+                    button.Background = Brushes.Aqua;
+
+                    lvHistory.Items.Add(number.ToString());
                 }
 
-                MessageBox.Show(history_str);
+                bool bingo = false;
+                // 縦方向に探索
+                for (int i = 0; i < 5; i++)
+                {
+                    int punched = 0;
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (table_state[i + j * 5]) { punched++; }
+                        else { break; }
+                    }
+                    if (punched == 5) { bingo = true; }
+                }
+                //横方向に探索
+                for (int i = 0; i < 5; i++)
+                {
+                    int punched = 0;
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (table_state[i * 5 + j]) { punched++; }
+                        else { break; }
+                    }
+                    if (punched == 5) { bingo = true; }
+                }
+
+                //斜め方向に探索
+                int punchedAngle = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    if (table_state[i * 6]) { punchedAngle++; }
+                    else { break; }
+                }
+                if (punchedAngle == 5) { bingo = true; }
+                
+                punchedAngle = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    if (table_state[(i + 1) * 4]) { punchedAngle++; }
+                    else { break; }
+                }
+                if (punchedAngle == 5) { bingo = true; }
+
+                if (bingo)
+                {
+                    MessageBox.Show("BINGO!");
+                }
             }
         }
     }
